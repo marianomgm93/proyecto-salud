5345f151c027b1618cb210418595501480868e2d
Julian Agustin Fernandez O <gonna1560@gmail.com>
2023-11-22 12:28:06 -0300

    creacion-entidad-usuario

--- salud/src/main/java/com/grupos/salud/entidades/Usuario.java
+++ salud/src/main/java/com/grupos/salud/entidades/Usuario.java
@@ -1,5 +1,61 @@
 package com.grupos.salud.entidades;
 
-public class Usuario {
+import com.grupos.salud.enumeraciones.Rol;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-}
+
+@Entity
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombreUsuario;
+    private String contraseña;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
+   
+    public Usuario() {
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return contraseña;
+    }
+
+    public void setPassword(String password) {
+        this.contraseña = password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    }
