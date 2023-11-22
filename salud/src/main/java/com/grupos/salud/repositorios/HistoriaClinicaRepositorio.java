934451cf55d30a9b2dd2721daa4459795cb17ea3
MatiasE003 <gamermaplex@gmail.com>
2023-11-22 20:51:02 -0300

    Update HistoriaClinicaRepositorio.java

--- salud/src/main/java/com/grupos/salud/repositorios/HistoriaClinicaRepositorio.java
+++ salud/src/main/java/com/grupos/salud/repositorios/HistoriaClinicaRepositorio.java
@@ -3,6 +3,7 @@ package com.grupos.salud.repositorios;
 import com.grupos.salud.entidades.HistoriaClinica;
 import com.grupos.salud.entidades.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -12,10 +13,13 @@ import java.util.Optional;
 @Repository
 public interface HistoriaClinicaRepositorio extends JpaRepository<HistoriaClinica, String> {
 
+    @Query("SELECT h FROM HistoriaClinica h WHERE h.fecha = :fecha")
     List<HistoriaClinica> findByFecha(Date fecha);
 
-    List<HistoriaClinica> findByProfesionalId(String id);
+    @Query("SELECT h FROM HistoriaClinica h WHERE h.profesional.id = :profesionalId")
+    List<HistoriaClinica> findByProfesionalId(String profesionalId);
 
+    @Query("SELECT h FROM HistoriaClinica h WHERE h.paciente = :paciente")
     Optional<HistoriaClinica> findByPaciente(Paciente paciente);
 
 }
