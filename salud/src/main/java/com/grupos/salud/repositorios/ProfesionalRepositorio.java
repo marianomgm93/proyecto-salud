140363166dc40e430a462861b7ca25ced5f9e148
MatiasE003 <gamermaplex@gmail.com>
2023-11-22 20:47:43 -0300

    Update ProfesionalRepositorio.java

--- salud/src/main/java/com/grupos/salud/repositorios/ProfesionalRepositorio.java
+++ salud/src/main/java/com/grupos/salud/repositorios/ProfesionalRepositorio.java
@@ -5,6 +5,7 @@ package com.grupos.salud.repositorios;
 
 import com.grupos.salud.entidades.Profesional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
