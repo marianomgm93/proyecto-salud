6097610815c0ae285558794c42a310fc13cc6f80
Mariano Gonzalez <marianomgm93@gmail.com>
2023-11-22 19:19:55 -0300

    HistoriaClinica entidad

--- salud/src/main/java/com/grupos/salud/entidades/HistoriaClinica.java
+++ salud/src/main/java/com/grupos/salud/entidades/HistoriaClinica.java
@@ -1,5 +1,67 @@
 package com.grupos.salud.entidades;
 
-public class HistoriaClinica {
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.GenericGenerator;
 
+@Entity
+public class HistoriaClinica implements Serializable {
+    @Id
+    @GeneratedValue(generator="uuid")
+    @GenericGenerator(name="uuid", strategy="uuid2")
+    private String id;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    
+    @ManyToOne
+    private Profesional profesional;
+    
+    @OneToOne
+    private Paciente paciente;
+
+    public HistoriaClinica() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Profesional getProfesional() {
+        return profesional;
+    }
+
+    public void setProfesional(Profesional profesional) {
+        this.profesional = profesional;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+    
+    
 }
