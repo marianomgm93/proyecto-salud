de51f4c0f6176d8a7a87c881ca069e753029b367
Nicolas Fiorito <nicolas_fiorito@outlook.com>
2023-11-22 19:27:42 -0300

    creacion de la entidad profesional

--- salud/src/main/java/com/grupos/salud/entidades/Profesional.java
+++ salud/src/main/java/com/grupos/salud/entidades/Profesional.java
@@ -1,5 +1,41 @@
 package com.grupos.salud.entidades;
 
-public class Profesional {
+import javax.persistence.Entity;
+
+@Entity
+public class Profesional extends Usuario{
+    
+    private String especialidad;
+    private Double reputacion;
+    private Double valorConsulta;
+
+    public Profesional() {
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Double getReputacion() {
+        return reputacion;
+    }
+
+    public void setReputacion(Double reputacion) {
+        this.reputacion = reputacion;
+    }
+
+    public Double getValorConsulta() {
+        return valorConsulta;
+    }
+
+    public void setValorConsulta(Double valorConsulta) {
+        this.valorConsulta = valorConsulta;
+    }
+    
+    
 
 }
