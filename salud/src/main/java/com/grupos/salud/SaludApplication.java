9168c01da42948030c08b63d990fa00533e439ea
Bruno <brunonahuel2013@gmail.com>
2023-11-22 19:36:32 -0300

    entidad turnos

--- salud/src/main/java/com/grupos/salud/SaludApplication.java
+++ salud/src/main/java/com/grupos/salud/SaludApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SaludApplication {
 
+    private String hola = "ed";
+    
 	public static void main(String[] args) {
 		SpringApplication.run(SaludApplication.class, args);
 	}
---
+++ salud/src/main/java/com/grupos/salud/com/grupos/salud/entidades/Turno.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.grupos.salud.com.grupos.salud.entidades;
+
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Turno {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaYHora;
+    
+    private String estado;
+
+    @OneToOne
+    private Paciente paciente;
+
+    @OneToOne
+    private Profesional profe;
+
+    
+
+
+}
