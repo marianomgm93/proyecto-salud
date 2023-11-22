bd2beb50d4faeb7193f91f05116d0b224d088129
Lucas Jaurena <lujaurena_2@yahoo.com.ar>
2023-11-22 20:02:21 -0300

    Actualizacion de archivos y creacion de la entidad Paciente

---
+++ salud/src/main/java/com/grupos/salud/entidades/Paciente.java
@@ -0,0 +1,59 @@
+package com.grupos.salud.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Paciente {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    private String datosContacto;
+    private String obraSocial;
+    
+    @OneToOne
+    private Imagen imagen;
+
+    public Paciente() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDatosContacto() {
+        return datosContacto;
+    }
+
+    public void setDatosContacto(String datosContacto) {
+        this.datosContacto = datosContacto;
+    }
+
+    public String getObraSocial() {
+        return obraSocial;
+    }
+
+    public void setObraSocial(String obraSocial) {
+        this.obraSocial = obraSocial;
+    }
+
+    public Imagen getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(Imagen imagen) {
+        this.imagen = imagen;
+    }
+    
+    
+    
+}
