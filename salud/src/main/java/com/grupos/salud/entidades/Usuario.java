bc4164f8d5a79fa86cc37dfdfda34cdfe496a056
Julian Agustin Fernandez O <gonna1560@gmail.com>
2023-11-23 20:33:20 -0300

    Cambios en usuario y usuario servicio

--- salud/src/main/java/com/grupos/salud/entidades/Usuario.java
+++ salud/src/main/java/com/grupos/salud/entidades/Usuario.java
@@ -18,6 +18,7 @@ public class Usuario implements Serializable {
     private String id;
 
     private String nombreUsuario;
+    private String email;
     private String password;
     private boolean estado;
 
@@ -41,6 +42,14 @@ public class Usuario implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     
