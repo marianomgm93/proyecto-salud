f118d7856c5b5db4ee2de4f65d2ad8f120952a10
MatiasE003 <gamermaplex@gmail.com>
2023-11-23 12:02:30 -0300

    Update HistoriaClinicaRepositorio.java
    saqué métodos que no se usaban y corregí el método findByFecha

--- salud/src/main/java/com/grupos/salud/repositorios/HistoriaClinicaRepositorio.java
+++ salud/src/main/java/com/grupos/salud/repositorios/HistoriaClinicaRepositorio.java
@@ -1,25 +1,19 @@
 package com.grupos.salud.repositorios;
 
 import com.grupos.salud.entidades.HistoriaClinica;
-import com.grupos.salud.entidades.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface HistoriaClinicaRepositorio extends JpaRepository<HistoriaClinica, String> {
 
     @Query("SELECT h FROM HistoriaClinica h WHERE h.fecha = :fecha")
-    List<HistoriaClinica> findByFecha(Date fecha);
+    public List<HistoriaClinica> findByFecha(@Param("fecha") Date fecha);
 
-    @Query("SELECT h FROM HistoriaClinica h WHERE h.profesional.id = :profesionalId")
-    List<HistoriaClinica> findByProfesionalId(String profesionalId);
-
-    @Query("SELECT h FROM HistoriaClinica h WHERE h.paciente = :paciente")
-    Optional<HistoriaClinica> findByPaciente(Paciente paciente);
 
 }
