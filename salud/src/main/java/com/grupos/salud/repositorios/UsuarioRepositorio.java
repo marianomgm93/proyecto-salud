aa94d6438f5220d2a28acd79d25bf305c676df61
Julian Agustin Fernandez O <gonna1560@gmail.com>
2023-11-23 13:49:24 -0300

    Se modifico usuario, se creo su repo y su servicio

--- salud/src/main/java/com/grupos/salud/repositorios/UsuarioRepositorio.java
+++ salud/src/main/java/com/grupos/salud/repositorios/UsuarioRepositorio.java
@@ -1,5 +1,20 @@
 package com.grupos.salud.repositorios;
 
-public class UsuarioRepositorio {
+import com.grupos.salud.entidades.Usuario;
+import com.grupos.salud.enumeraciones.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario, String> {
+
+    @Query("SELECT u FROM Usuario u WHERE u.nombreUsuario = :nombreUsuario")
+    public Usuario buscarPorNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
+
+    @Query("SELECT u FROM Usuario u WHERE u.rol = :rol")
+    public Usuario buscarPorRol(@Param("rol") Rol rol);
+    
+    
 }
