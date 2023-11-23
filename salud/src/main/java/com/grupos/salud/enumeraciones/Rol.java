e7537c97b3f6a790f840e0cd3028a79c1e161167
nicolas <nicolas_fiorito@outlook.com>
2023-11-23 19:56:39 -0300

    creacion de portalcontrolador y enum

--- salud/src/main/java/com/grupos/salud/enumeraciones/Rol.java
+++ salud/src/main/java/com/grupos/salud/enumeraciones/Rol.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
 package com.grupos.salud.enumeraciones;
 
-/**
- *
- * @author Hammerforce
- */
 public enum Rol {
+    
+    ADMIN,
+    USER;
     
 }
