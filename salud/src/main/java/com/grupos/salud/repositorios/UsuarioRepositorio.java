0fd8fe8a58f4e12074db16cd38ce942cd8817567
Mariano Gonzalez <marianomgm93@gmail.com>
2023-11-23 18:21:53 -0300

    23nov estable

--- salud/src/main/java/com/grupos/salud/repositorios/UsuarioRepositorio.java
+++ salud/src/main/java/com/grupos/salud/repositorios/UsuarioRepositorio.java
@@ -1,7 +1,7 @@
 package com.grupos.salud.repositorios;
 
+import com.grupos.salud.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepositorio extends JpaRepository{
-
+public interface UsuarioRepositorio extends JpaRepository<Usuario,String>{
 }
