9befa3a619b66ccb2c2f2e32b430b1ab7d76a924
Mariano Gonzalez <marianomgm93@gmail.com>
2023-11-23 18:59:15 -0300

    usuario patch

--- salud/src/main/java/com/grupos/salud/repositorios/UsuarioRepositorio.java
+++ salud/src/main/java/com/grupos/salud/repositorios/UsuarioRepositorio.java
@@ -1,8 +1,9 @@
 package com.grupos.salud.repositorios;
 
-
 import com.grupos.salud.entidades.Usuario;
 import com.grupos.salud.enumeraciones.Rol;
+import com.grupos.salud.entidades.Usuario;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, String> {
-
     @Query("SELECT u FROM Usuario u WHERE u.nombreUsuario = :nombreUsuario")
     public Usuario buscarPorNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
 
