2706c61a01ad662cb688f5f53f89d6e15fb8dc32
Mauro Rostirolla <mauro.autor@gmail.com>
2023-11-22 21:50:04 -0300

    TurnoRepositorio

--- salud/src/main/java/com/grupos/salud/repositorios/TurnoRepositorio.java
+++ salud/src/main/java/com/grupos/salud/repositorios/TurnoRepositorio.java
@@ -1,14 +1,13 @@
 package com.grupos.salud.repositorios;
 
 import com.grupos.salud.entidades.Turno;
-import java.util.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface TurnoRepositorio extends JpaRepository<Turno, String>{
     
-    @Query("SELECT t FROM Turno t WHERE t.fechaYHora = :fechaYHora")
-    public Turno buscarPorFecha(@Param("fechaYHora")Date fechaYHora);
+    @Query("SELECT e FROM Turno e WHERE e.estado = :estado")
+    public Turno buscarPorEstado(@Param("estado")String estado);
     
 }
