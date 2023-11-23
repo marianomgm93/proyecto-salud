e3b694ebbe03085067d287c9d2de9abd9392d7a3
ValenCalog <127156207+ValenCalog@users.noreply.github.com>
2023-11-23 00:18:48 -0300

    Paciente servicio

--- salud/src/main/java/com/grupos/salud/servicios/PacienteServicio.java
+++ salud/src/main/java/com/grupos/salud/servicios/PacienteServicio.java
@@ -4,6 +4,8 @@ package com.grupos.salud.servicios;
 import com.grupos.salud.entidades.Paciente;
 import com.grupos.salud.excepciones.MiException;
 import com.grupos.salud.repositorios.PacienteRepositorio;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +16,6 @@ public class PacienteServicio {
     @Autowired
     private PacienteRepositorio pacienteRepositorio;
     
-        /* private String datosContacto;
-           private String obraSocial;
-    
-           @OneToOne
-            private Imagen imagen;
-        */
     
     @Transactional
     public void registrar(String datosContacto, String obraSocial) throws MiException{
@@ -29,7 +25,30 @@ public class PacienteServicio {
         paciente.setObraSocial(obraSocial);
         pacienteRepositorio.save(paciente);
     }
-   
+    
+    @Transactional(readOnly=true)
+    public List<Paciente> listarPacientes(){
+        List<Paciente> pacientes = pacienteRepositorio.findAll();
+        return pacientes;
+    }
+            
+    
+    public Paciente getOne(String id){
+        Paciente paciente = pacienteRepositorio.getOne(id);
+        return paciente;
+    } 
+    
+    @Transactional
+    public void actualizar(String id, String nuevosDatosContacto, String nuevaObraSocial) throws MiException{
+        validar(nuevosDatosContacto, nuevaObraSocial);
+        Optional<Paciente> respuesta = pacienteRepositorio.findById(id);
+        if(respuesta.isPresent()){
+            Paciente paciente = respuesta.get();
+            paciente.setDatosContacto(nuevosDatosContacto);
+            paciente.setObraSocial(nuevaObraSocial);
+            pacienteRepositorio.save(paciente);
+        }
+    }
     
     private void validar(String datosContacto,String obraSocial) throws MiException{
         if(datosContacto.isEmpty() || datosContacto == null){
