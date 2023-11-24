4625ad59d13802d64a5f2e1f4a384cd3fa9ccdbb
nicolas <nicolas_fiorito@outlook.com>
2023-11-24 17:37:57 -0300

    se agrego modal de login desde index

--- salud/src/main/java/com/grupos/salud/SeguridadWeb.java
+++ salud/src/main/java/com/grupos/salud/SeguridadWeb.java
@@ -44,7 +44,7 @@ public class SeguridadWeb extends WebSecurityConfigurerAdapter {
                         .permitAll()
                 .and().logout()
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
+                        .logoutSuccessUrl("/")
                         .permitAll()
                 .and().csrf()
                         .disable();
