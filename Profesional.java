6b62aedfd7be77315f165e93338fa9857cdcd6e6
maurogaxton <mauro.autor@gmail.com>
2023-11-24 22:06:14 -0300

    Servicio Profesional

---
+++ Profesional.java
@@ -0,0 +1,66 @@
+package com.grupos.salud.entidades;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+public class Profesional {
+    @Id
+    @GeneratedValue(generator="uuid")
+    @GenericGenerator(name="uuid",strategy="uuid2")
+    
+    private String id;
+    private String especialidad;
+    private Double reputacion;
+    private Double valorConsulta;
+    private Boolean estado;
+    
+    public Profesional() {
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Double getReputacion() {
+        return reputacion;
+    }
+
+    public void setReputacion(Double reputacion) {
+        this.reputacion = reputacion;
+    }
+
+    public Double getValorConsulta() {
+        return valorConsulta;
+    }
+
+    public void setValorConsulta(Double valorConsulta) {
+        this.valorConsulta = valorConsulta;
+    }
+    
+}
