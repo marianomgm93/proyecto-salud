daf0304840d119e487aa89e08874e03274c9063c
MatiasE003 <gamermaplex@gmail.com>
2023-11-28 10:58:54 -0300

    Create Ficha.java

---
+++ salud/src/main/java/com/grupos/salud/entidades/Ficha.java
@@ -0,0 +1,77 @@
+package com.grupos.salud.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Ficha {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @OneToOne
+    private Paciente paciente;
+
+    @OneToOne
+    private Profesional profesional;
+
+    private String diagnostico;
+
+    private Boolean estado;
+
+    public Ficha() {
+    }
+
+    public Ficha(Paciente paciente, Profesional profesional, String diagnostico, Boolean estado) {
+        this.paciente = paciente;
+        this.profesional = profesional;
+        this.diagnostico = diagnostico;
+        this.estado = estado;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Paciente getPaciente() {
+        return this.paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Profesional getProfesional() {
+        return this.profesional;
+    }
+
+    public void setProfesional(Profesional profesional) {
+        this.profesional = profesional;
+    }
+
+    public String getDiagnostico() {
+        return this.diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public Boolean getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
